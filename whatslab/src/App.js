950cc401adab3e44a9f9ecbd3f09e1762eccf7a3
unknown <gustavogst01@gmail.com>
2021-05-03 11:59:27 -0300

    Foi excluido componentes e adicionado UM com todos dentro

---
+++ whatslab/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import InputDoUsuario from './InputDoUsuario';
+
+
+function App() {
+  return (
+      <div className ="App">
+        <div className="page-section-container">
+            <InputDoUsuario 
+            
+            />
+          </div>
+       </div>   
+  );
+}
+
+export default App;
